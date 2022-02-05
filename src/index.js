d5e2384446ce3734b72b34e96db19bb95bc5109a
Bijoy Francis <bijoyfrancis@gmail.com>
2022-02-05 14:29:49 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ async function run () {
   try {
     core.debug((new Date()).toTimeString()) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     core.info(github.context.repo.owner)
+    core.info(github.context.repo.name)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
